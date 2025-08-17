9ad87af58255a7a3e4927f85519257ea2e642f61
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-08-17 12:31:38 +0530

    escape to close the webcam working

--- volume-controller.py
+++ volume-controller.py
@@ -3,15 +3,18 @@ import mediapipe as mp
 import pyautogui
 
 webcam = cv2.VideoCapture(0)
-
 while True:
-    _ , image = webcam.read()
-    cv2.imshow("Hand volume control using Python", image)
-    cv2.waitKey(10)
-    
-    key = cv2.waitKey(1)
+    ret, image = webcam.read()
+    if not ret:
+        print("Failed to grab the frame")
+        break
+
+    cv2.imshow("Hand Volume Control using Python", image)
+
+    key= cv2.waitKey(10)
     if key == 27:
         break
 
-    webcam.release()
-    cv2.destroyAllWindows()
+webcam.release()
+cv2.destroyAllWindows()
+# Lets see if this works now or not...
