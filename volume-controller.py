bb37405ad04f086c9be3fdded450da226c8ded3b
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-08-17 12:19:05 +0530

    okoklalala

---
+++ volume-controller.py
@@ -0,0 +1,10 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+
+webcam = cv2.VideoCapture(0)
+
+while True:
+    _ , image = webcam.read()
+    cv2.imshow("Hand volume control using Python", image)
+    cv2.waitKey(10)
