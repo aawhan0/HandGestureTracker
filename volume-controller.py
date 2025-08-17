aa255627dbc1badd77ff75b2b4ea788d8b82c856
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-08-17 12:20:14 +0530

    webcam close added

--- volume-controller.py
+++ volume-controller.py
@@ -8,3 +8,8 @@ while True:
     _ , image = webcam.read()
     cv2.imshow("Hand volume control using Python", image)
     cv2.waitKey(10)
+    if key == 27:
+        break
+
+    webcam.release()
+    cv2.destroyAllWindows()
