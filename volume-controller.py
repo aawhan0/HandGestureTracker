c47b8d37b479b00d2167cdc6a1bacb2c5072e111
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-08-17 12:22:00 +0530

    key define

--- volume-controller.py
+++ volume-controller.py
@@ -7,7 +7,9 @@ webcam = cv2.VideoCapture(0)
 while True:
     _ , image = webcam.read()
     cv2.imshow("Hand volume control using Python", image)
-    cv2.waitKey(10)
+    cv2.waitKey(10
+    
+    key = cv2.waitKey(1)
     if key == 27:
         break
 
